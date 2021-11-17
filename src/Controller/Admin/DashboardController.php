f2142bc35809d1facc45cd7837b64ae6f5706942
Michael Käfer <michael.kaefer1@gmx.at>
2021-11-17 11:15:16 +0100

    Finish basic setup to use as base for reproducers

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -26,7 +26,6 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-//        yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+//        yield MenuItem::linkToCrud('Entity', 'fas fa-list', EntityClass::class);
     }
 }
